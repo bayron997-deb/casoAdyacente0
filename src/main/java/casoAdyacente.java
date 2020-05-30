01a9b9b15de0860635991c9f7da7fdab68bc6318
Bayron <bayron.m.1999@gmail.com>
2020-05-30 16:56:04 -0400

    se crea un validador para ingresar numeros en el arreglo

--- src/main/java/casoAdyacente.java
+++ src/main/java/casoAdyacente.java
@@ -1,4 +1,7 @@
+import java.util.Scanner;
+
 public class casoAdyacente {
+    public static Scanner teclado=new Scanner(System.in);
     public static int prodAdyacente(int[]numeros){
         int result=0;
         for (int i=0;i<numeros.length-1;++i){
@@ -7,5 +10,17 @@ public class casoAdyacente {
             }
         }
         return result;
+    }
+    public static void validador(){
+        System.out.println("Ingrese cantidad de numeros a comparar: ");
+        int largo=teclado.nextInt();
+        int [] numeros=new int[largo];
+
+        System.out.println("Ingrese numeros: ");
+        for (int i=0; i<numeros.length;++i){
+            numeros[i]=teclado.nextInt();
+        }
+        int resultado=prodAdyacente(numeros);
+        System.out.println("mayor: "+resultado);
     }
 }
