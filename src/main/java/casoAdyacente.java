132841dbdc755bc3c45896595323ebdb38345fb9
Fernando <f.fernandez10@ufromail.cl>
2020-05-30 17:51:54 -0400

    se agrega inputo de numeros 'double'

--- src/main/java/casoAdyacente.java
+++ src/main/java/casoAdyacente.java
@@ -1,9 +1,11 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class casoAdyacente {
-    public static Scanner teclado=new Scanner(System.in);
-    public static int prodAdyacente(int[]numeros){
-        int result=0;
+    public static Scanner teclado =new Scanner(System.in);
+
+    public static double prodAdyacente(double[]numeros){
+        double result=0;
         for (int i=0;i<numeros.length-1;++i){
             if (numeros[i]*numeros[i+1]>result) {
                 result = numeros[i] * numeros[i + 1];
@@ -13,14 +15,58 @@ public class casoAdyacente {
     }
     public static void arreglo(){
         System.out.println("Ingrese cantidad de numeros a comparar: ");
-        int largo=teclado.nextInt();
-        int [] numeros=new int[largo];
+        //el largo toma el valor de el metodo
+        int largo = validarint();
+        double  [] numeros=new double[largo];
 
-        System.out.println("Ingrese numeros: ");
         for (int i=0; i<numeros.length;++i){
-            numeros[i]=teclado.nextInt();
+            System.out.println("Ingrese un numero ");
+            //cada numero se valida
+            numeros[i]=validardouble();
         }
-        int resultado=prodAdyacente(numeros);
+        double resultado=prodAdyacente(numeros);
         System.out.println("mayor: "+resultado);
     }
-}
+
+    public static int validarint(){
+        boolean repetir = true;
+        int a = 0;
+        while(repetir){
+            try{
+                a = teclado.nextInt();
+                if(a<2) {
+                    a=2;
+                }else if(a>20){
+                    a=20;
+                }
+                repetir = false;
+            } catch (InputMismatchException e){
+                teclado.next(); // Limpia el Scanner
+                System.out.println("Ingrese un largo valido");
+                repetir = true;
+            }
+        }
+        return a;
+    }
+    public static double  validardouble(){
+        boolean repetir = true;
+        double a = 0;
+        while(repetir){
+            try{
+                a = teclado.nextDouble();
+                if(a<-1000){
+                    a=-1000;
+                }else if (a>1000){
+                    a=1000;
+                }
+                repetir = false;
+            } catch (InputMismatchException e){
+                teclado.next(); // Limpia el Scanner
+                System.out.println("Ingrese un número (valido), por favor");
+                repetir = true;
+            }
+        }
+        return a;
+    }
+
+    }
